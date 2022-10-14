3c3ca61587e00a7e53e6d9d0bcc0d6fd0a7f9cb3
Michal Válka <michal.valka@warxim.com>
2022-10-14 14:48:45 +0200

    CVE-2022-41852 Proof of Concept

---
+++ src/main/java/com/warxim/jxpathrce/JXPathRCEApplication.java
@@ -0,0 +1,13 @@
+package com.warxim.jxpathrce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JXPathRCEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JXPathRCEApplication.class, args);
+	}
+
+}
